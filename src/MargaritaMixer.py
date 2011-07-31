1c8162a6427c1b35f10b93c1bc5d0da071081801
johnm <devnull@localhost>
2011-07-31 14:04:17 -0700

    Initial commit of working PyQt QTest Example

---
+++ src/MargaritaMixer.py
@@ -0,0 +1,81 @@
+# vim: set fileencoding=utf-8 :
+# $Header:  $
+#
+# Copyright 2011 Voom, Inc.
+#
+# This file is part of the Voom PyQt QTest Example.
+# See http://www.voom.net/pyqt-qtest-example/ for documentation.
+#
+# PyQt QTest Example is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# PyQt QTest Example is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with PyQt QTest Example.  If not, see <http://www.gnu.org/licenses/>.
+
+"""
+GUI for a margarita mixing machine.
+Extract Ui_MargaritaMixer.py using:
+   pyuic4 --output Ui_MargaritaMixer.py MargaritaMixer.ui
+"""
+
+__author__ = "John McGehee, http://www.voom.net/"
+__version__ = "$Revision: 1.0 $"
+__date__ = "$Date: 2011/07/30 $"
+__copyright__ = "Copyright 2011 Voom, Inc."
+
+import sys
+from PyQt4 import QtGui, QtCore
+
+from Ui_MargaritaMixer import Ui_MargaritaMixer
+
+class MargaritaMixer(QtGui.QWidget):
+  
+    def __init__(self):
+        super(MargaritaMixer, self).__init__()
+        
+        self.ui = Ui_MargaritaMixer()
+        self.ui.setupUi(self)
+
+    def getJiggers(self):
+        '''Return the total volume of the margaritas in units of jiggers.
+        One jigger is 0.0444 liters.
+        '''
+        jiggersTequila = self.ui.tequilaScrollBar.value()
+        jiggersTripleSec = self.ui.tripleSecSpinBox.value()
+        jiggersLimeJuice = self.ui.limeJuiceLineEdit.text().toDouble()[0]
+        jiggersIce = self.ui.iceHorizontalSlider.value()
+        return jiggersTequila + jiggersTripleSec + jiggersLimeJuice + jiggersIce
+    
+    def getLiters(self):
+        '''Return the total volume of the margaritas in liters.'''
+        return 0.0444 * self.jiggers
+        
+    def getSpeedName(self):
+        speedButton = self.ui.speedButtonGroup.checkedButton()
+        if speedButton is None:
+            return None
+        return speedButton.text()
+
+    def accept(self):
+        '''Execute the command in response to the OK button.'''
+        print 'The volume of drinks is {0} liters ({1} jiggers).'.format(self.getLiters(), self.getJiggers())
+        print 'The blender is running at speed "{0}"'.format(self.getSpeedName())
+        self.close()
+
+    def reject(self):
+        '''Cancel.'''
+        self.close()
+
+
+if __name__ == "__main__":
+    app = QtGui.QApplication(sys.argv)
+    myapp = MargaritaMixer()
+    myapp.show()
+    sys.exit(app.exec_())
---
+++ src/MargaritaMixerTest.py
@@ -0,0 +1,155 @@
+# vim: set fileencoding=utf-8 :
+# $Header:  $
+#
+# Copyright 2011 Voom, Inc.
+#
+# This file is part of the Voom PyQt QTest Example.
+# See http://www.voom.net/pyqt-qtest-example/ for documentation.
+#
+# PyQt QTest Example is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# PyQt QTest Example is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with PyQt QTest Example.  If not, see <http://www.gnu.org/licenses/>.
+
+"""
+Test the margarita mixer GUI.
+"""
+
+__author__ = "John McGehee, http://www.voom.net/"
+__version__ = "$Revision: 1.0 $"
+__date__ = "$Date: 2011/07/30 $"
+__copyright__ = "Copyright 2011 Voom, Inc."
+
+import sys
+import unittest
+from PyQt4.QtGui import QApplication
+from PyQt4.QtTest import QTest
+from PyQt4.QtCore import Qt
+
+import MargaritaMixer
+
+
+class MargaritaMixerTest(unittest.TestCase):
+    '''Test the margarita mixer GUI'''
+    def setUp(self):
+        '''Create the GUI'''
+        self.app = QApplication(sys.argv)
+        self.form = MargaritaMixer.MargaritaMixer()
+
+    def setFormToZero(self):
+        '''Set all ingredients to zero in preparation for setting just one
+        to a nonzero value.
+        '''
+        self.form.ui.tequilaScrollBar.setValue(0)
+        self.form.ui.tripleSecSpinBox.setValue(0)
+        self.form.ui.limeJuiceLineEdit.setText("0.0")
+        self.form.ui.iceHorizontalSlider.setValue(0)
+
+    def test_defaults(self):
+        '''Test the GUI in its default state'''
+        self.assertEqual(self.form.ui.tequilaScrollBar.value(), 8)
+        self.assertEqual(self.form.ui.tripleSecSpinBox.value(), 4)
+        self.assertEqual(self.form.ui.limeJuiceLineEdit.text(), "12.0")
+        self.assertEqual(self.form.ui.iceHorizontalSlider.value(), 12)
+        self.assertEqual(self.form.ui.speedButtonGroup.checkedButton().text(), "&Karate Chop")
+
+        # Class is in the default state even without pressing OK
+        self.assertEqual(self.form.getJiggers(), 36.0)
+        self.assertEqual(self.form.getSpeedName(), "&Karate Chop")
+        
+        # Push OK with the left mouse button
+        okWidget = self.form.ui.buttonBox.button(self.form.ui.buttonBox.Ok)
+        QTest.mouseClick(okWidget, Qt.LeftButton)
+        self.assertEqual(self.form.getJiggers(), 36.0)
+        self.assertEqual(self.form.getSpeedName(), "&Karate Chop")
+        
+
+    def test_tequilaScrollBar(self):
+        '''Test the tequila scroll bar'''
+        self.setFormToZero()
+        
+        # Test the maximum.  This one goes to 11.
+        self.form.ui.tequilaScrollBar.setValue(12)
+        self.assertEqual(self.form.ui.tequilaScrollBar.value(), 11)
+
+        # Test the minimum of zero.
+        self.form.ui.tequilaScrollBar.setValue(-1)
+        self.assertEqual(self.form.ui.tequilaScrollBar.value(), 0)
+
+        self.form.ui.tequilaScrollBar.setValue(5)
+        
+        # Push OK with the left mouse button
+        okWidget = self.form.ui.buttonBox.button(self.form.ui.buttonBox.Ok)
+        QTest.mouseClick(okWidget, Qt.LeftButton)
+        self.assertEqual(self.form.getJiggers(), 5)
+   
+    def test_tripleSecSpinBox(self):
+        '''Test the triple sec spin box.
+        Testing the minimum and maximum is left as an exercise for the reader.
+        '''
+        self.setFormToZero()
+        self.form.ui.tripleSecSpinBox.setValue(2)
+
+        # Push OK with the left mouse button
+        okWidget = self.form.ui.buttonBox.button(self.form.ui.buttonBox.Ok)
+        QTest.mouseClick(okWidget, Qt.LeftButton)
+        self.assertEqual(self.form.getJiggers(), 2)
+        
+    def test_limeJuiceLineEdit(self):
+        '''Test the lime juice line edit.
+        Testing the minimum and maximum is left as an exercise for the reader.
+        '''
+        self.setFormToZero()
+        # Clear and then type "3.5" into the lineEdit widget
+        self.form.ui.limeJuiceLineEdit.clear()
+        QTest.keyClicks(self.form.ui.limeJuiceLineEdit, "3.5")
+
+        # Push OK with the left mouse button
+        okWidget = self.form.ui.buttonBox.button(self.form.ui.buttonBox.Ok)
+        QTest.mouseClick(okWidget, Qt.LeftButton)
+        self.assertEqual(self.form.getJiggers(), 3.5)
+
+    def test_iceHorizontalSlider(self):
+        '''Test the ice slider.
+        Testing the minimum and maximum is left as an exercise for the reader.
+        '''
+        self.setFormToZero()
+        self.form.ui.iceHorizontalSlider.setValue(4)
+
+        # Push OK with the left mouse button
+        okWidget = self.form.ui.buttonBox.button(self.form.ui.buttonBox.Ok)
+        QTest.mouseClick(okWidget, Qt.LeftButton)
+        self.assertEqual(self.form.getJiggers(), 4)
+        
+    def test_blenderSpeedButtons(self):
+        '''Test the blender speed buttons'''
+        self.form.ui.speedButton1.click()
+        self.assertEqual(self.form.getSpeedName(), "&Mix")
+        self.form.ui.speedButton2.click()
+        self.assertEqual(self.form.getSpeedName(), "&Whip")
+        self.form.ui.speedButton3.click()
+        self.assertEqual(self.form.getSpeedName(), "&Puree")
+        self.form.ui.speedButton4.click()
+        self.assertEqual(self.form.getSpeedName(), "&Chop")
+        self.form.ui.speedButton5.click()
+        self.assertEqual(self.form.getSpeedName(), "&Karate Chop")
+        self.form.ui.speedButton6.click()
+        self.assertEqual(self.form.getSpeedName(), "&Beat")
+        self.form.ui.speedButton7.click()
+        self.assertEqual(self.form.getSpeedName(), "&Smash")
+        self.form.ui.speedButton8.click()
+        self.assertEqual(self.form.getSpeedName(), "&Liquefy")
+        self.form.ui.speedButton9.click()
+        self.assertEqual(self.form.getSpeedName(), "&Vaporize")
+
+
+if __name__ == "__main__":
+    unittest.main()
